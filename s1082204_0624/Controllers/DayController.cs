66f296459410ec3a9daa7c070abc52067c0bd5bc
黃重皓 <sokve855@gmail.com>
2021-06-24 20:04:26 +0800

    finish

---
+++ s1082204_0624/Controllers/DayController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace s1082204_0624.Controllers
+{
+    public class DayController : Controller
+    {
+        // GET: Day
+        public ActionResult Index(float? weight)
+        {
+            var result = "";
+
+            
+
+            if (weight.HasValue)
+            {
+                result = "星期"+ (weight);  
+            }
+            ViewBag.result = result;
+
+            return View();
+        }
+    }
+}
