c96901a23860550b95c48ab481b18ac04c79673c
黃重皓 <sokve855@gmail.com>
2021-06-24 18:59:33 +0800

    exam/about create

---
+++ s1082204_0624/Controllers/ExamController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace s1082204_0624.Controllers
+{
+    public class ExamController : Controller
+    {
+        // GET: Exam
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
